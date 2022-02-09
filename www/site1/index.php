fc41dfb1e1b7cf45162ec80e8d224ed3fd662f91
Zapasnoekoleso <kiryusha.ilyasov@mail.ru>
2022-02-09 14:14:03 +0300

    init

---
+++ www/site1/index.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+<h1>site1.docker.ru</h1>
+<?php
+phpinfo();
+?>
+</body>
+</html>
---
+++ www/site2/index.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+<h1>site2.docker.ru</h1>
+<?php
+phpinfo();
+?>
+</body>
+</html>
